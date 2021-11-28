90ee117d89bd435047aea6a4fec7d3dbe9884bb1
ChengYan@Mac <hu_yancheng@outlook.com>
2021-11-28 02:58:50 -0500

    LSD radix sort

--- src/main/java/edu/neu/coe/info6205/finalproject/LSDSort.java
+++ src/main/java/edu/neu/coe/info6205/finalproject/LSDSort.java
@@ -1,12 +1,9 @@
 package edu.neu.coe.info6205.finalproject;
 
-import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class LSDSort {
+
     public static void sort(String[] a, int W)  {
         int R = 256;
         int N = a.length;
@@ -24,67 +21,146 @@ public class LSDSort {
                 a[i] = aux[i];
         }
     }
-//    List<List<String>> pinyinNames = getPinyinNames(ChineseNames);
 
-    // pad pinyin to same length
-//    String[] name = PinyinUtil.getPinYinByName()
-//
-//    List<String> entirePinyinNames = new ArrayList<>(ChineseNames.length);
-//        for (List<String> pinyinName : pinyinNames) {
-//        StringBuilder sb = new StringBuilder();
-//        for (String s : pinyinName) {
-//            int diff = maxCharLength - s.length();
-//            sb.append(s);
-////                for (int k = 0; k < diff; k++)
-////                    sb.append(padding);
-////                sb.append(s);
-//        }
-//        maxChineseNameLength = Math.max(maxChineseNameLength, sb.length());
-//        entirePinyinNames.add(sb.toString());
-//    }
-//        for (int i = 0; i < entirePinyinNames.size(); i++) {
-//        StringBuilder sb = new StringBuilder();
-//        int diff = maxChineseNameLength - entirePinyinNames.get(i).length();
-////            sb.append(entirePinyinNames.get(i));
-//        for (int k = 0; k < diff; k++)
-//            sb.append(padding);
-//        sb.append(entirePinyinNames.get(i));
-//        entirePinyinNames.set(i, sb.toString());
-//    }
+    /**
+     * Sort Chinese by LSD Radix algorithm.
+     * @param a string array of Chinese names.
+     */
+    public static void sortChinese(String[] a) {
+        int[] map = new int[a.length];
+        for (int i = 0; i < map.length; i++)
+            map[i] = i;
+        String[] paddedPinyins = preprocessData(a);
+        sort(paddedPinyins, maxLenOfPinyin, map);
+        String[] rawA = new String[a.length];
+        System.arraycopy(a, 0, rawA, 0, a.length);
+        for (int i = 0; i < a.length; i++)
+            a[i] = rawA[map[i]];
 
-    static int maxLen = 0;
+    }
 
-    private static String[] getPaddingName(String[] names) {
-        List<String> pinyinNames = new ArrayList<>();
-        Arrays.stream(names).forEach(s-> {
-            try {
-                String str = PinyinUtil.getPinYinByName(s);
-                maxLen = Math.max(maxLen, str.length());
-                pinyinNames.add(PinyinUtil.getPinYinByName(s));
-            } catch (BadHanyuPinyinOutputFormatCombination e) {
-                e.printStackTrace();
-            }
+    /**
+     * Radix sort by pinyin.
+     * @param a string array of pinyin.
+     * @param W maximum string length of @param a.
+     * @param map map to raw pinyin.
+     *            1 -> ["zhang1", "san1"]
+     *            2 -> ["li3", "si4"]
+     *            3 -> ["zhao4", "wu3"]
+     */
+    private static void sort(String[] a, int W, int[] map) {
+        int R = 256;
+        int N = a.length;
+        int[] aux = new int[N];
+//        System.out.println(W);
+        for (int d = W-1; d >= 0; d--)
+        {
+//            System.out.println("-----------------------" + d);
+//            for (int i = 0; i < a.length; i++)
+//                System.out.println(a[map[i]]);
+//            System.out.println("-----------------------");
+            int[] count = new int[R+1];
+            for (int i = 0; i < N; i++)
+                count[a[map[i]].charAt(d)+1]++;
+            for (int r = 0; r < R; r++)
+                count[r+1] += count[r];
+            for (int i = 0; i < N; i++)
+                aux[count[a[map[i]].charAt(d)]++] = map[i];
+            for (int i = 0; i < N; i++)
+                map[i] = aux[i];
+        }
+    }
+
+    private static int maxLen = 0; // max length of name
+
+    private static final char paddingChar = '#'; // padding character (less than all alphabet and tones)
+
+    private static final int maxLengthOfChar = 7; // maximum length of pinyin with tone (e.x. zhuang1)
+
+    private static int maxLenOfPinyin = 0; // max length of pinyin of a name.
+
+    /**
+     * Get pinyin.
+     * @param names string array of Chinese names.
+     * @return a list of pinyin name.
+     *         [
+     *          ["zhang1", "san1"],
+     *          ["li3", "si4"],
+     *          ["zhao4", "wu3"],
+     *         ]
+     */
+    private static List<String[]> getPinyin(String[] names) {
+        maxLen=0;
+        List<String[]> pinyins = new ArrayList<>(names.length);
+        Arrays.stream(names).forEach(name -> {
+            String[] pinyin = PinyinUtil.getPinyinWithVWithTone(name);
+            if (maxLen < pinyin.length)
+                maxLen = pinyin.length;
+            pinyins.add(pinyin);
         });
+        return pinyins;
+    }
 
-        return pinyinNames.toArray(new String[0]);
+    /**
+     * TODO: get max length in another way.
+     * Get maximum length of pinyin.
+     * @param names array of pinyin names.
+     * @return int maximum length of pinyin.
+     */
+    private static int getMaxLengthOfPinyin(String[] names) {
+        int maxLen=0;
+        for (String name : names)
+            if (maxLen < name.length())
+                maxLen = name.length();
+
+        return maxLen;
+    }
+
+    private static String[] preprocessData(String[] ChineseNames) {
+        List<String[]> pinyins = getPinyin(ChineseNames);
+        maxLenOfPinyin = maxLengthOfChar * maxLen;
+        // padding
+        return pad(pinyins);
+    }
+
+    /**
+     * Pad all pinyin name with @param paddingChar.
+     * @param pinyins list of pinyin.
+     * @return string array of padded pinyin.
+     */
+    private static String[] pad(List<String[]> pinyins) {
+        String[] padded = new String[pinyins.size()];
+        for (int i = 0; i < pinyins.size(); i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < pinyins.get(i).length; j++) {
+                String pinyinOfChar = pinyins.get(i)[j];
+                sb.append(pinyinOfChar);
+                for (int k = pinyinOfChar.length(); k < maxLengthOfChar; k++)
+                    sb.append(paddingChar);
+            }
+            for (int j = sb.length(); j < maxLenOfPinyin; j++)
+                sb.append(paddingChar);
+            padded[i] = sb.toString();
+        }
+
+        return padded;
     }
 
     public static void main(String[] args) {
-        String[] zhongwen = {"张三", "李四", "赵武"};
-        String[] name = getPaddingName(zhongwen);
-        String[] padded = new String[zhongwen.length];
-        for (int i = 0; i < name.length; i++) {
-            StringBuilder sb = new StringBuilder();
-            int diff = maxLen-name[i].length();
-            sb.append(name[i]);
-            for (int j = 0; j < diff; j++) {
-                sb.append('`');
-            }
-            padded[i] = sb.toString();
-        }
-        sort(padded, maxLen);
-        Arrays.stream(padded).forEach(System.out::println);
+        String[] zhongwen = FileUtil.getAllNamesToArray();
+//        String[] zhongwen = {"詹丽娟","詹玉琴","翟姗姗","詹华丽","翟世超","占福","詹志远","占英","翟少杰","展炜"};
+        sortChinese(zhongwen);
+        Arrays.stream(zhongwen).forEach(System.out::println);
+
+//        String[] zhongwen1 = {"詹丽娟","詹玉琴","翟姗姗","詹华丽","翟世超","占福","詹志远","占英","翟少杰","展炜"};
+//        String[] pinyin = pad(getPinyin(zhongwen1));
+//        Arrays.stream(pinyin).forEach(System.out::println);
+//        sort(pinyin, maxLenOfPinyin);
+//        Arrays.stream(zhongwen1).forEach(System.out::println);
     }
 
-
 }
+/**
+ * 5321100
+ * 82
+ */
--- src/main/java/edu/neu/coe/info6205/finalproject/PinyinUtil.java
+++ src/main/java/edu/neu/coe/info6205/finalproject/PinyinUtil.java
@@ -1,58 +1,16 @@
 package edu.neu.coe.info6205.finalproject;
 
 import net.sourceforge.pinyin4j.PinyinHelper;
-import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
 import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
 import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
 import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
 import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 
+import java.util.Arrays;
+
 public class PinyinUtil {
 
-//    public static void main(String[] args) throws BadHanyuPinyinOutputFormatCombination {
-//
-////        String str = PinyinUtil.getPinYinHeadChar("小超人");
-////        System.out.println(str);
-//
-////        String[] strs = PinyinUtil.getPinYinWithToneMark("小");
-////        for (String str : strs) {
-////            System.out.println(str);
-////        }
-//        getPinYinByName("的发射点");
-//    }
-
-    /**
-     * 传入中文获取首字母 （小写）
-     * 如：小超人 -> xcr
-     *
-     * @param str 需要转化的中文字符串
-     * @return
-     */
-    public static String getPinYinHeadChar(String str) {
-        String convert = "";
-        for (int j = 0; j < str.length(); j++) {
-            char word = str.charAt(j);
-            String[] pinyinArray = PinyinHelper.toHanyuPinyinStringArray(word);
-            if (pinyinArray != null) {
-                convert += pinyinArray[0].charAt(0);
-            } else {
-                convert += word;
-            }
-        }
-        return convert;
-    }
-
-
-//    /**
-//     * 获取中文字的拼音（多音字，拼音后的数字代表第几声）
-//     * 如：空 -> kong1 kong4
-//     *
-//     * @param word
-//     * @return
-//     */
-//    public static String[] getPinYin(char word) {
-//        return PinyinHelper.toHanyuPinyinStringArray(word);
-//    }
+    private static final HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
 
     /**
      * 获取中文字的拼音（多音字，拼音上的符号代表第几声）
@@ -61,21 +19,55 @@ public class PinyinUtil {
      * @param word
      * @return
      */
-
     public static String[] getPinYinWithToneMark(char word) throws BadHanyuPinyinOutputFormatCombination {
-        HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
         format.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
         format.setVCharType(HanyuPinyinVCharType.WITH_U_UNICODE);
         return PinyinHelper.toHanyuPinyinStringArray(word, format);
     }
 
-    public static String getPinYinByName(String name) throws BadHanyuPinyinOutputFormatCombination {
+    public static String getPinYinByName(String name) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < name.length(); i++) {
-            sb.append(getPinYinWithToneMark(name.charAt(i))[0]);
+            try {
+                sb.append(getPinYinWithToneMark(name.charAt(i))[0]);
+            } catch (BadHanyuPinyinOutputFormatCombination badHanyuPinyinOutputFormatCombination) {
+                badHanyuPinyinOutputFormatCombination.printStackTrace();
+            }
         }
         //System.out.println(sb.toString());
         return sb.toString();
+    }
+
+    /**
+     * get pinyin in V char type and tone number.
+     * @param Chinese Chinese
+     * @return string array of pinyin
+     *         张三 -> ["zhang1", "san1"]
+     */
+    public static String[] getPinyinWithVWithTone(String Chinese) {
+        // set format
+        format.setToneType(HanyuPinyinToneType.WITH_TONE_NUMBER);
+        format.setVCharType(HanyuPinyinVCharType.WITH_V);
+
+        String[] pinyin = new String[Chinese.length()];
+        for (int i = 0; i < Chinese.length(); i++) {
+            try {
+                pinyin[i] = PinyinHelper.toHanyuPinyinStringArray(Chinese.charAt(i), format)[0];
+            } catch (BadHanyuPinyinOutputFormatCombination badHanyuPinyinOutputFormatCombination) {
+                badHanyuPinyinOutputFormatCombination.printStackTrace();
+            }
+        }
+
+        return pinyin;
+    }
+
+    public static void main(String[] args) {
+        String[] zhongwen = {"张三", "李四", "赵武"};
+        Arrays.stream(zhongwen).forEach(name -> {
+            String[] pinyin = getPinyinWithVWithTone(name);
+            Arrays.stream(pinyin).forEach(s -> System.out.print(s + " "));
+            System.out.println();
+        });
     }
 
 }
