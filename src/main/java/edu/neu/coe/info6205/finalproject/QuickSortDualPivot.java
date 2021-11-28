f50a1d7fc8e69f8a4333a6c62e5a2a8ee2f14acc
Weifeng Lai <laiweifeng51@gmail.com>
2021-11-27 20:58:14 -0500

    Add comparison via Collator

--- src/main/java/edu/neu/coe/info6205/finalproject/QuickSortDualPivot.java
+++ src/main/java/edu/neu/coe/info6205/finalproject/QuickSortDualPivot.java
@@ -1,8 +1,13 @@
 package edu.neu.coe.info6205.finalproject;
 
+import java.text.Collator;
+import java.util.Locale;
+
 public class QuickSortDualPivot {
 
-    private static void sort(Comparable[] a, int lo, int hi) {
+    private static Collator collator = Collator.getInstance(Locale.CHINA);
+
+    private static void sort(String[] a, int lo, int hi) {
         if (hi <= lo) return;
 
         // make sure a[lo] <= a[hi]
@@ -23,8 +28,16 @@ public class QuickSortDualPivot {
         sort(a, gt + 1, hi);
     }
 
-    private static boolean less(Comparable a, Comparable b) {
-        if (a.compareTo(b) < 0) {
+//    private static boolean less(String a, String b) {
+//        if (a.compareTo(b) < 0) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+
+    private static boolean less(String s1, String s2) {
+        if (collator.compare(s1, s2) < 0) {
             return true;
         } else {
             return false;
@@ -39,26 +52,42 @@ public class QuickSortDualPivot {
 
     // Read strings from standard input, sort them, and print.
     public static void main(String[] args) {
-        String[] zhongwen = {"张三", "李四", "赵武"};
-        String[] pinyin = new String[3];
-        for (int i = 0; i < zhongwen.length; i++) {
-            try {
-                pinyin[i] = PinyinUtil.getPinYinByName(zhongwen[i]);
-            } catch (Exception e) {
-                System.out.println("error");
-            }
-        }
+        String[] cnNames = {"张三", "李四", "扎炸", "赵武"};
 
-        for (String s : pinyin) {
+        /* Sort by chinese name  */
+        for (String s : cnNames) {
             System.out.println(s);
         }
         System.out.println("-------------------------------------");
 
-        QuickSortDualPivot.sort(pinyin, 0, pinyin.length - 1);
+        QuickSortDualPivot.sort(cnNames, 0, cnNames.length - 1);
 
-        for (String s : pinyin) {
+        for (String s : cnNames) {
             System.out.println(s);
         }
         System.out.println("-------------------------------------");
+
+//        /* Sort by pinyin */
+//        String[] pinyin = new String[3];
+//        for (int i = 0; i < cnNames.length; i++) {
+//            try {
+//                pinyin[i] = PinyinUtil.getPinYinByName(cnNames[i]);
+//                pinyin[i] = cnNames[i];
+//            } catch (Exception e) {
+//                System.out.println("error");
+//            }
+//        }
+//
+//        for (String s : pinyin) {
+//            System.out.println(s);
+//        }
+//        System.out.println("-------------------------------------");
+//
+//        QuickSortDualPivot.sort(pinyin, 0, pinyin.length - 1);
+//
+//        for (String s : pinyin) {
+//            System.out.println(s);
+//        }
+//        System.out.println("-------------------------------------");
     }
 }
